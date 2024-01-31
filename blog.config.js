0f9560e43fa6b843faee7b7b53437f6a8f3ead97
zxxxxz <291233773@qq.com>
2024-01-31 20:41:40 +0800

    modify description

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: '-zxxxxz-',
   email: '291233773@qq.com',
   link: 'https://nobelium.vercel.app',
-  description: 'You Only Live Once.',
+  description: 'Attention is all you need.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
